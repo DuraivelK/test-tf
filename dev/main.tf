f10a0d834b659b590eb04b4e08db9b9b43057368
DuraivelK <dkalyanasundaram@forbesadvisor.com>
2023-04-11 15:25:45 +0530

    Removed s3 resource

--- dev/main.tf
+++ dev/main.tf
@@ -13,11 +13,3 @@ terraform {
 provider "aws" {
   region            = "us-east-1"
 }
-
-resource "aws_s3_bucket" "test-tf-dev-bkt" {
-  bucket        = "test-tf-dev-bkt" 
-  tags = {
-    Name        = "test-tf-dev-bkt"
-    Environment = "dev"
-  }
-}
